78688e84793bd9534478f5323551b4e8a6599873
gokutek <vxer@qq.com>
2018-04-09 13:22:27 +0800

    增加Test项目

---
+++ Test/main.cpp
@@ -0,0 +1,20 @@
+﻿// 临时测试代码
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <list>
+#include <memory>
+#include <initializer_list>
+#include <stack>
+#include <fstream>
+
+
+int main()
+{
+	std::cout << "hello" << std::endl;
+	return 0;
+}
