3ab2f6e9a2685bdd6ab50219058a0b5295d0886b
gokutek <vxer@qq.com>
2018-04-10 16:22:47 +0800

    引用

--- Test/main.cpp
+++ Test/main.cpp
@@ -11,12 +11,27 @@
 #include <initializer_list>
 #include <stack>
 #include <fstream>
+#include <assert.h>
+
+
+template<typename T>
+void test_ref(T val)
+{
+	val = 10;
+}
+
+
+void test_ref_()
+{
+	int a = 100;
+	int& ra = a;
+	test_ref(ra);
+	assert(ra == 100);
+}
 
 
 int main()
 {
-	std::cout << "hello" << std::endl;
-
 	const char* sz = "hello"; // 指针指向的是常量
 	sz = "world"; // 可以修改指针本身以指向其它对象
 	//*sz = 'a'; // error C3892: “sz”: 不能给常量赋值
