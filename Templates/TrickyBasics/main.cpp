18bfbd05daf0df9ae0ddf666022aa5d095d1e4c1
gokutek <vxer@qq.com>
2018-04-11 08:28:30 +0800

    .template构造

---
+++ Templates/TrickyBasics/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+template<typename T>
+class Shell
+{
+public:
+	template<int N>
+	class In
+	{
+	public:
+		template<int M>
+		class Deep
+		{
+		public:
+			void f()
+			{
+				std::cout << __FUNCTION__ << ": M = " << M << std::endl;
+			}
+		};
+	};
+};
+
+
+template<typename T, int N>
+class Weird
+{
+public:
+	//void case1(Shell<T>::In<N>::Deep<N>* p) // 错误
+	//void case1(typename Shell<T>::In<N>::Deep<N>* p) // 错误
+	void case1(typename Shell<T>::template In<N>::template Deep<N>* p)
+	{
+		//p->template Deep<N>::f(); // 书上是这种写法，然后现在编译不过了。
+		p->f();
+		p->Deep<N>::f();
+	}
+};
+
+
+int main()
+{
+	Shell<int>::In<5>::Deep<5> deep;
+	Weird<int, 5> w;
+	w.case1(&deep);
+
+	return 0;
+}
