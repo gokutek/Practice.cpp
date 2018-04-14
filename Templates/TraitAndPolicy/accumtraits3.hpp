cde427d7e58c8a74beec237ade7df329c822de43
gokutek <vxer@qq.com>
2018-04-14 11:59:07 +0800

    trait

---
+++ Templates/TraitAndPolicy/accumtraits3.hpp
@@ -0,0 +1,42 @@
+﻿#ifndef ACCUMTRAITS2_HPP
+#define ACCUMTRAITS2_HPP
+
+template<typename T>
+class AccumTraits;
+
+template<>
+class AccumTraits<int>
+{
+public:
+	typedef int Acct;
+};
+
+template<>
+class AccumTraits<char>
+{
+public:
+	typedef int Acct;
+};
+
+
+//template<>
+//class AccumTraits<float>
+//{
+//public:
+//	typedef float Acct;
+//};
+
+
+template<typename T>
+inline typename AccumTraits<T>::Acct accum3(T const* beg, T const* end)
+{
+	typedef typename AccumTraits<T>::Acct TotalType;
+	TotalType total = TotalType();
+	while (beg != end) {
+		total += *beg;
+		++beg;
+	}
+	return total;
+}
+
+#endif // ACCUMTRAITS2_HPP
---
+++ Templates/TraitAndPolicy/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "accum1.hpp"
+#include "accumtraits3.hpp"
+
+
+int main()
+{
+	int num[] = { 1, 2, 3, 4, 5 };
+	std::cout << accum1(num, num + 5) << std::endl;
+
+	// 当T被推导为char时，total在内部累加时会溢出,
+	char name[] = "abcd";
+	std::cout << accum1(name, name + strlen(name)) << std::endl;
+
+	// 上面问题的一种解决方案是，增加一个模板参数，由用户传入这个类型参数作为total变量的类型
+	std::cout << accum2<int>(name, name + strlen(name)) << std::endl;
+
+	// 比较好的办法，由traits推导返回值类型，不需要用户自己传
+	std::cout << accum3(num, num + 5) << std::endl;
+	std::cout << accum3(name, name + strlen(name)) << std::endl;
+
+	// 没有AccumTraits<float>特化，所以下面的代码编译失败
+	//float fNum[] = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
+	//std::cout << accum3(fNum, fNum + 5) << std::endl;
+	return 0;
+}
