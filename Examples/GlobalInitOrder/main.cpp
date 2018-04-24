d07346e220ce60850093fb7af4439ec255e80104
gokutek <vxer@qq.com>
2018-04-24 10:23:52 +0800

    全局变量初始化顺序

---
+++ Examples/GlobalInitOrder/main.cpp
@@ -0,0 +1,35 @@
+﻿/*
+===============================================================================
+POD类型的全局变量先初始化
+===============================================================================
+*/
+#include <iostream>
+
+struct MyStruct
+{
+public:
+	MyStruct(int x)
+		: val(x)
+	{
+		static int count = 1;
+		std::cout << "count = " << count << std::endl;
+		++count;
+
+		extern int g_IntVal;
+		std::cout << "g_IntVal = " << g_IntVal << std::endl;
+		++g_IntVal;
+	}
+
+	int val;
+};
+
+MyStruct	g_MyStuct1(1);
+int g_IntVal = 888;
+MyStruct	g_MyStuct2(2);
+
+
+int main()
+{
+	std::cout << "g_IntVal = " << g_IntVal << std::endl;
+	return 0;
+}
