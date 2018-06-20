6f20a5ad1d7d35fe54ff892be2c85b3180b9d32e
gokutek <vxer@qq.com>
2018-06-20 11:48:25 +0800

    htons

---
+++ Examples/Socket/main.cpp
@@ -0,0 +1,20 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <WinSock2.h>
+#include <windows.h>
+
+
+int main()
+{
+    WSADATA data;
+    WSAStartup(MAKEWORD(2, 2), &data);
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(55566); // 不能直接写55566
+    int ret = bind(sock, (sockaddr*)&addr, sizeof(addr));
+    ret = listen(sock, 100);
+    WSACleanup();
+    return 0;
+}
