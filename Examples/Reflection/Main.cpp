d8c0a768a8dabf82ee90dcb3603a7785493dc8ab
gokutek <vxer@qq.com>
2018-07-09 10:43:36 +0800

    测试，支持float类型

--- Examples/Reflection/Main.cpp
+++ Examples/Reflection/Main.cpp
@@ -5,6 +5,7 @@ struct Node
 {
     std::string key;
     int value;
+    float fVal;
     std::vector<Node> children;
 
     REFLECT()       // Enable reflection for this type
@@ -15,6 +16,7 @@ struct Node
 REFLECT_STRUCT_BEGIN(Node)
 REFLECT_STRUCT_MEMBER(key)
 REFLECT_STRUCT_MEMBER(value)
+REFLECT_STRUCT_MEMBER(fVal)
 REFLECT_STRUCT_MEMBER(children)
 REFLECT_STRUCT_END()
 
@@ -22,7 +24,7 @@ REFLECT_STRUCT_END()
 int main() 
 {
     // Create an object of type Node
-    Node node = {"apple", 3, {{"banana", 7, {}}, {"cherry", 11, {}}}};
+    Node node = {"apple", 3, 1.23f, {{"banana", 7, {}}, {"cherry", 11, {}}}};
 
     // Find Node's type descriptor
     reflect::TypeDescriptor* typeDesc = reflect::TypeResolver<Node>::get();
@@ -32,3 +34,11 @@ int main()
 
     return 0;
 }
+
+
+/*
+===============================================================================
+说明：
+这个DEMO只能用来反射类的成员变量。
+===============================================================================
+*/
--- Examples/Reflection/Primitives.cpp
+++ Examples/Reflection/Primitives.cpp
@@ -28,6 +28,30 @@ TypeDescriptor* getPrimitiveDescriptor<int>()
 
 
 //--------------------------------------------------------
+// A type descriptor for float
+//--------------------------------------------------------
+struct TypeDescriptor_Float : TypeDescriptor
+{
+    TypeDescriptor_Float() : TypeDescriptor{ "float", sizeof(float) }
+    {
+    }
+
+    virtual void dump(const void* obj, int /* unused */) const override
+    {
+        std::cout << "float{" << *(const float*)obj << "}";
+    }
+};
+
+
+template <>
+TypeDescriptor* getPrimitiveDescriptor<float>()
+{
+    static TypeDescriptor_Float typeDesc;
+    return &typeDesc;
+}
+
+
+//--------------------------------------------------------
 // A type descriptor for std::string
 //--------------------------------------------------------
 struct TypeDescriptor_StdString : TypeDescriptor
