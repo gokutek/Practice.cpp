84ab2519a7e49fe27271eb01fdbc52d56d00cf57
gokutek <vxer@qq.com>
2018-08-03 09:22:44 +0800

    typeid测试

--- Test/main.cpp
+++ Test/main.cpp
@@ -72,9 +72,88 @@ static void test_bitset()
     std::cout << bs.to_string() << std::endl;
 }
 
+class Animal 
+{
+public:
+    virtual ~Animal() {}
+};
+
+class Cat : public Animal
+{
+public:
+};
+
+
+static void test_typeid()
+{
+    {
+        int x = 0;
+        const type_info& info = typeid(&x);
+        std::cout << "name: " << info.name() << std::endl;
+        std::cout << "hash_code: " << info.hash_code() << std::endl;
+        std::cout << "raw_name: " << info.raw_name() << std::endl;
+    }
+
+    {
+        std::cout << std::endl;
+        std::vector<int> vec;
+        const type_info& info = typeid(&vec);
+        std::cout << "name: " << info.name() << std::endl;
+        std::cout << "hash_code: " << info.hash_code() << std::endl;
+        std::cout << "raw_name: " << info.raw_name() << std::endl;
+    }
+
+    {
+        std::cout << std::endl << "Cat cat;" << std::endl;
+        std::cout << "typeid(&cat);" << std::endl;
+        Cat cat;
+        const type_info& info = typeid(&cat);
+        std::cout << "name: " << info.name() << std::endl;
+        std::cout << "hash_code: " << info.hash_code() << std::endl;
+        std::cout << "raw_name: " << info.raw_name() << std::endl;
+    }
+
+    {
+        std::cout << std::endl << "Cat cat;" << std::endl;
+        std::cout << "typeid(cat);" << std::endl;
+        Cat cat;
+        const type_info& info = typeid(cat);
+        std::cout << "name: " << info.name() << std::endl;
+        std::cout << "hash_code: " << info.hash_code() << std::endl;
+        std::cout << "raw_name: " << info.raw_name() << std::endl;
+    }
+
+    {
+        std::cout << std::endl << "Cat cat;" << std::endl;
+        std::cout << "Animal &animal = cat;" << std::endl;
+        std::cout << "typeid(animal);" << std::endl;
+        Cat cat;
+        Animal &animal = cat;
+        const type_info& info = typeid(animal);
+        std::cout << "name: " << info.name() << std::endl;
+        std::cout << "hash_code: " << info.hash_code() << std::endl;
+        std::cout << "raw_name: " << info.raw_name() << std::endl;
+    }
+
+    {
+        std::cout << std::endl << "Cat cat;" << std::endl;
+        std::cout << "Animal *animal = &cat;" << std::endl;
+        std::cout << "typeid(animal);" << std::endl;
+        Cat cat;
+        Animal *animal = &cat;
+        const type_info& info = typeid(animal);
+        std::cout << "name: " << info.name() << std::endl;
+        std::cout << "hash_code: " << info.hash_code() << std::endl;
+        std::cout << "raw_name: " << info.raw_name() << std::endl;
+    }
+}
+
 
 int main()
 {
+    test_typeid();
+    return -1;
+
     test_bitset();
     return -1;
 
