0680105c66d2ac56088251a0cfd5a8107d5a6a3d
gokutek <vxer@qq.com>
2018-08-06 09:56:18 +0800

    kapok原理

---
+++ Examples/Serialization/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+
+
+struct Person
+{
+    std::string name;
+    int         age;
+
+    /*
+    ===========================================================================
+    META宏展开后生成如下的成员函数，是一个tuple，其中每一项又是一个pair，包含成员变
+    量的名字以及引用
+    ===========================================================================
+    */
+    auto Meta()
+    {
+        return std::make_tuple(
+            std::make_pair("name", std::reference_wrapper<decltype(name)>(name)),
+            std::make_pair("age", std::reference_wrapper<decltype(age)>(age)));
+    }
+};
+
+
+int main()
+{
+    // Kapok库的核心原理
+    Person person;
+    person.name = "Jim";
+    person.age = 23;
+
+    auto meta = person.Meta();
+    auto p = std::get<0>(meta);
+    p.second = "Lucy";
+
+    return 0;
+}
