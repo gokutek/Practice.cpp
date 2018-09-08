2318f6c0edeaf2cdb89024108d0f6013e00fcb37
gokutek <vxer@qq.com>
2018-09-08 12:43:04 +0800

    catch

--- Examples/CatchUnitTestExample/main.cpp
+++ Examples/CatchUnitTestExample/main.cpp
@@ -7,5 +7,6 @@
 1. https://github.com/catchorg/Catch2/blob/master/docs/tutorial.md；
 2. 可以在工程设置里将[C/C++]、[预处理器]中的预处理到文件打开，看宏展开后的代码；
 3. 为每个TEST CASE生成唯一函数名：__COUNTER__；
+4. https://github.com/catchorg/Catch2/blob/master/docs/own-main.md；
 ===============================================================================
 */
