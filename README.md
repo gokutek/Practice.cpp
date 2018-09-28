a324bf84e731cf38fcf4b659222392eee3cbc972
gokutek <vxer@qq.com>
2018-09-28 20:57:56 +0800

    ECS

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ C++ Practice
 ### C++ Templates
 
 * P248：“我们并不能对函数模板预设缺省模板实参”，vs2015中已支持
+
+
+### ECS
+
+https://github.com/Unity-Technologies/EntityComponentSystemSamples/
+https://github.com/alecthomas/entityx
+http://cowboyprogramming.com/2007/01/05/evolve-your-heirachy/
---
+++ ThirdParty/entityx-1.1.2/examples/test.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include "entityx/quick.h"
+
+
+struct DebugSystem : public entityx::System<DebugSystem>
+{
+    void update(entityx::EntityManager &entities, entityx::EventManager &events, entityx::TimeDelta dt) override
+    {
+    }
+};
+
+
+struct MovementSystem : public entityx::System<DebugSystem>
+{
+    void update(entityx::EntityManager &entities, entityx::EventManager &events, entityx::TimeDelta dt) override
+    {
+    }
+};
+
+
+struct CollisionSystem : public entityx::System<DebugSystem>
+{
+    void update(entityx::EntityManager &entities, entityx::EventManager &events, entityx::TimeDelta dt) override
+    {
+    }
+};
+
+
+class Level : public entityx::EntityX
+{
+public:
+    Level()
+    {
+        systems.add<DebugSystem>();
+        systems.add<MovementSystem>();
+        systems.add<CollisionSystem>();
+        systems.configure();
+    }
+
+
+    void update(float dt)
+    {
+        systems.update<DebugSystem>(dt);
+        systems.update<MovementSystem>(dt);
+        systems.update<CollisionSystem>(dt);
+    }
+};
+
+
+int main()
+{
+    Level level;
+    while (true) {
+        level.update(0.1f);
+    }
+    return 0;
+}
