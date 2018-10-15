24a37241822cd7581bd67e47c92bf7a4e8ac4473
gokutek <vxer@qq.com>
2018-10-15 13:37:21 +0800

    bitset

--- Examples/Tmp/test_bitset.cpp
+++ Examples/Tmp/test_bitset.cpp
@@ -2,10 +2,28 @@
 #include <iostream>
 #include "catch.hpp"
 
+/*
+===================
+“位运算”
+===================
+*/
 
 TEST_CASE("bitset", "[bitset]")
 {
     std::bitset<32> bs(0xfe); // 1111 1110
     REQUIRE(bs.count() == 7);
     REQUIRE(bs.to_string() == "00000000000000000000000011111110");
+
+    REQUIRE(bs.size() == 32);
+
+    REQUIRE(!bs.test(0));
+
+    bs.set(0, true);
+    REQUIRE(bs.test(0));
+
+    bs.reset();
+    REQUIRE(bs.to_string() == "00000000000000000000000000000000");
+
+    bs.set();
+    REQUIRE(bs.to_string() == "11111111111111111111111111111111");
 }
