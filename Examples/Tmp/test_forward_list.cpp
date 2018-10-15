958632770f17d707227ab3e1a2a89cdfd18c9072
gokutek <vxer@qq.com>
2018-10-15 11:55:48 +0800

    std::forward_list

---
+++ Examples/Tmp/test_forward_list.cpp
@@ -0,0 +1,11 @@
+﻿#include <forward_list>
+#include "catch.hpp"
+
+
+TEST_CASE("misc", "[std::forward_list]")
+{
+    std::forward_list<int> l;
+    
+    l.push_front(1);
+    REQUIRE(l.front() == 1);
+}
