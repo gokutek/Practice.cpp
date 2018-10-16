b878b50ac3b13412ead5025273e6be124e36c9a5
gokutek <vxer@qq.com>
2018-10-16 11:26:24 +0800

    C++ templates complete guide 2nd

---
+++ Books/C++ templates complete guide 2nd/1_max1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include "catch.hpp"
+
+
+namespace
+{
+	struct Foo
+	{
+		int val;
+
+		Foo(int v) : val(v)
+		{
+			//std::cout << __FUNCTION__ << std::endl;
+		}
+
+		Foo(Foo const &other) : val(other.val)
+		{
+			//std::cout << __FUNCTION__ << std::endl;
+		}
+
+		~Foo()
+		{
+			//std::cout << __FUNCTION__ << std::endl;
+		}
+
+		bool operator<(Foo const &other) const
+		{
+			return val < other.val;
+		}
+	};
+}
+
+
+template<typename T>
+T max1(T a, T b)
+{
+	return b < a ? a : b;
+}
+
+
+TEST_CASE("max1.hpp", "basic")
+{
+	Foo f1(1);
+	Foo f2(2);
+
+	// 模板被实例化为：Foo max1(Foo a, Foo b)，所以在调用过程中，会有对象拷贝
+	auto res = max1(f1, f2);
+	REQUIRE(res.val == 2);
+}
---
+++ Books/C++ templates complete guide 2nd/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+
+int main(int argc, char* argv[])
+{
+    return Catch::Session().run(argc, argv);
+}
