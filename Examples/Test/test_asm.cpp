79290fbac2a608f22fd870ad77dc92652caf8eed
gokutek <vxer@qq.com>
2018-10-23 14:30:37 +0800

    内联汇编

---
+++ Examples/Test/test_asm.cpp
@@ -0,0 +1,49 @@
+﻿#ifdef _MSC_VER
+
+#include "catch.hpp"
+
+/*
+========
+内联汇编
+========
+*/
+
+TEST_CASE("assembly.1", "[ASM]")
+{
+	int count = 12;
+
+	__asm
+	{
+		mov eax, count
+		mov ebx, 85
+		add eax, ebx
+		mov count, eax
+	}
+
+	REQUIRE(count == 97);
+}
+
+
+static int func1(int a, int b)
+{
+	return a + b;
+}
+
+
+TEST_CASE("assembly.2", "[ASM]")
+{
+	int res;
+
+	__asm
+	{
+		push 12
+		push 13
+		call func1
+		mov res, eax
+		add esp, 8
+	}
+
+	REQUIRE(res == 25);
+}
+
+#endif // _MSC_VER
