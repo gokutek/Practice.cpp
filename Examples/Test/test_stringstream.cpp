8cf80237ca8b8655a1b745f0b8eb535f13a936cf
gokutek <vxer@qq.com>
2018-10-24 09:55:10 +0800

    test_stringstream

---
+++ Examples/Test/test_stringstream.cpp
@@ -0,0 +1,18 @@
+﻿#include <sstream>
+#include "catch.hpp"
+
+/*
+=====================
+sstream中相关类的使用
+=====================
+*/
+
+TEST_CASE("sstream clear", "sstream")
+{
+	std::ostringstream oss;
+	oss << "hello";
+	oss.clear();
+	oss << "world";
+
+	REQUIRE(oss.str() == "helloworld");
+}
