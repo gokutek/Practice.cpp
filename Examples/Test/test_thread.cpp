9210703031e8b5669c1b18181c863fbaec006a1b
gokutek <vxer@qq.com>
2018-10-24 17:44:32 +0800

    thread

--- Examples/Test/test_thread.cpp
+++ Examples/Test/test_thread.cpp
@@ -49,3 +49,39 @@ TEST_CASE("thread", "[thread]")
     t2.join();
     REQUIRE(N == 15);
 }
+
+
+TEST_CASE("mutex", "[thread]")
+{
+	std::mutex mutex;
+	REQUIRE(mutex.try_lock());
+	mutex.unlock();
+}
+
+
+/*
+===============================================================================
+std::lock_guard与std::unique_lock的区别：
+The difference is that you can lock and unlock a std::unique_lock. 
+std::lock_guard will be locked only once on construction and unlocked on destruction.
+
+std::unique_lock has other features that allow it to e.g.: be constructed 
+without locking the mutex immediately but to build the RAII wrapper. However, 
+std::unique_lock might have a tad more overhead.
+
+std::lock_guard also provides a convenient RAII wrapper, but cannot lock
+multiple mutexes safely. It can be used when you need a wrapper for a limited
+scope, e.g.: a member function.
+
+One of the differences between std::lock_guard and std::unique_lock is that
+the programmer is able to unlock std::unique_lock, but she/he is not able to
+unlock std::lock_guard.
+===============================================================================
+*/
+
+
+TEST_CASE("lock_guard", "[thread]")
+{
+	std::mutex mutex;
+	std::lock_guard<std::mutex> guard(mutex);
+}
