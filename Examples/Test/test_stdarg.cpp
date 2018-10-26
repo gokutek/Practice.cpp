e162ab286d35cb128378d65fea8cbda2cf5cf516
gokutek <vxer@qq.com>
2018-10-26 13:33:14 +0800

    stdarg

---
+++ Examples/Test/test_stdarg.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdarg.h>
+#include "catch.hpp"
+
+
+static std::string my_printf(char const *fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	char buffer[512];
+	vsprintf(buffer, fmt, args);
+	va_end(args);
+	return buffer;
+}
+
+
+TEST_CASE("va_list", "stdarg")
+{
+	std::string const str = my_printf("hello %d %s", 123, "world");
+	REQUIRE(str == "hello 123 world");
+}
