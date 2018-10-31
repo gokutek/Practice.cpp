68de8372606f5f1189019441c27cbc9f9f9fee3b
gokutek <vxer@qq.com>
2018-10-31 13:15:34 +0800

    time

---
+++ Examples/Test/test_time.cpp
@@ -0,0 +1,30 @@
+﻿#include <time.h>
+#include "catch.hpp"
+
+
+/*
+=========================
+time的输入参数和返回值相同
+=========================
+*/
+TEST_CASE("time", "time")
+{
+	time_t arg;
+	time_t now = time(&arg);
+	REQUIRE(arg == now);
+}
+
+
+/*
+================================
+可以使用strftime来格式化时间字符串
+================================
+*/
+TEST_CASE("strftime", "time")
+{
+	time_t now = time(NULL);
+	tm *ltm = localtime(&now);
+	char strTime[64];
+	strftime(strTime, sizeof(strTime), "%F %T", ltm);
+	REQUIRE(strlen(strTime) == strlen("2018-10-31 13:08:42"));
+}
