fa7a7c7266d4d2a52ad40cc97cdcd98175798e9c
gokutek <vxer@qq.com>
2018-11-02 16:07:37 +0800

    win32

--- Examples/Test/test_os_windows.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <Windows.h>
-#include "catch.hpp"
-
-/*
-========================
-Windows系统API的一些测试
-========================
-*/
---
+++ Examples/Test/test_win_api.cpp
@@ -0,0 +1,32 @@
+﻿#include <Windows.h>
+#include "catch.hpp"
+
+/*
+========================
+Windows系统API的一些测试
+========================
+*/
+
+/*
+===============================================================================
+创建进程时，可以让新进程处于suspended状态，之后使用ResumeThread让新进程开始运行
+===============================================================================
+*/
+TEST_CASE("CreateProcess", "WinAPI")
+{
+	wchar_t args[512] = L"notepad.exe";
+	STARTUPINFO si;
+	memset(&si, 0, sizeof(si));
+	si.cb = sizeof(si);
+	PROCESS_INFORMATION pi;
+	memset(&pi, 0, sizeof(pi));
+	BOOL bRet = CreateProcessW(NULL, args, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi);
+	REQUIRE(bRet != FALSE);
+
+	ResumeThread(pi.hThread);
+
+	TerminateProcess(pi.hProcess, 0);
+
+	CloseHandle(pi.hThread);
+	CloseHandle(pi.hProcess);
+}
