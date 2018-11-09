6c6bc8e3c3660f820629b983a9bf8b9cfcf8dff8
gokutek <vxer@qq.com>
2018-11-09 09:24:54 +0800

    std::any

---
+++ Examples/Test/test_any.cpp
@@ -0,0 +1,37 @@
+﻿#include <any>
+#include <string>
+#include "catch.hpp"
+
+
+namespace
+{
+	class Foo
+	{
+	public:
+		Foo(int val) : value(val)
+		{
+		}
+
+		~Foo()
+		{
+		}
+
+		int value;
+	};
+}
+
+
+TEST_CASE("any", "[std::any]")
+{
+	std::any a;
+	a = std::string("123");
+
+	std::string const str = std::any_cast<std::string>(a);
+	REQUIRE(str == "123");
+
+	a = Foo(123);
+	REQUIRE(std::any_cast<Foo>(a).value == 123);
+
+	std::any_cast<Foo&>(a).value = 666;
+	REQUIRE(std::any_cast<Foo>(a).value == 666);
+}
