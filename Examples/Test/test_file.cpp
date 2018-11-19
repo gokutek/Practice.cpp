ddb9f40f01bb0b70c587c56087af49ad6b82ef60
gokutek <vxer@qq.com>
2018-11-19 14:01:05 +0800

    目录遍历

---
+++ Examples/Test/test_file.cpp
@@ -0,0 +1,20 @@
+﻿#include <io.h>
+#include "catch.hpp"
+
+
+/*
+====================
+目录遍历，涉及3个API:
+_findfirst
+_findnext
+_findclose
+====================
+*/
+TEST_CASE("os.walk", "io")
+{
+	_finddata_t data;
+	intptr_t handle = _findfirst("C:\\*", &data);
+	REQUIRE(-1 != handle);
+	REQUIRE(0 == _findnext(handle, &data));
+	_findclose(handle);
+}
