f56cfd5e57f8f1f829b8397f87609ab8cad523a8
gokutek <vxer@qq.com>
2022-04-21 10:35:32 +0800

    sqlite测试

--- Examples/Test/test_sqlite.cpp
+++ Examples/Test/test_sqlite.cpp
@@ -20,11 +20,13 @@ int main()
 	assert(errorno == SQLITE_OK);
 	char* errormsg = nullptr;
 
+	//删除表
+	errorno = sqlite3_exec(db, "drop table chat_rec_1000", nullptr, nullptr, nullptr);
 	//创建表
-	errorno = sqlite3_exec(db, "create table chat_rec_1000(id integer primary key autoincrement,name varchar(100))", nullptr, nullptr, nullptr);
+	errorno = sqlite3_exec(db, "create table chat_rec_1000(id integer primary key autoincrement,sender_uid varchar(100),sender_name varchar(100),message_body varchar(100),message_meta varchar(100))", nullptr, nullptr, nullptr);
 	//插入数据
-	errorno = sqlite3_exec(db, "insert into chat_rec_1000(name) values(\"hello\")", nullptr, nullptr, &errormsg);
-	errorno = sqlite3_exec(db, "insert into chat_rec_1000(name) values(\"world\")", nullptr, nullptr, nullptr);
+	errorno = sqlite3_exec(db, "insert into chat_rec_1000(sender_uid,sender_name,message_body,message_meta) values('e45678','tom','how old are you', '')", nullptr, nullptr, nullptr);
+	errorno = sqlite3_exec(db, "insert into chat_rec_1000(sender_uid,sender_name,message_body,message_meta) values('9876ac','jerry','i am 3 years old', '')", nullptr, nullptr, nullptr);
 	//查询表的所有行
 	errorno = sqlite3_exec(db, "select * from chat_rec_1000", select_callback, nullptr, nullptr);
 	//删除表中所有行
