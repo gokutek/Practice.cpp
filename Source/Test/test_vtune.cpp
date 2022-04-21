f03469730050385fbd3ea2aaa51c65548a1e66c3
gokutek <vxer@qq.com>
2022-04-21 10:43:07 +0800

    目录整理：vtune

--- Source/Test/test_vtune.cpp
+++ Source/Test/test_vtune.cpp
@@ -1,4 +1,14 @@
-﻿#include <windows.h>
+﻿/*
+===============================================================================
+1. VTune的安裝目录下有include、lib32、lib64几个目录，将它们将到搜索路径中去；
+2. 链接库的输入里加上：libittnotify.lib；
+2. 包含头文件："ittnotify.h"；
+3. To be able to see user tasks in your results, enable the Analyze user tasks checkbox in analysis settings；
+4. API的用法可参考帮助文档：[User's Guide]、[API Support]；
+===============================================================================
+*/
+
+#include <windows.h>
 #include <tchar.h>
 
 //#define INTEL_NO_ITTNOTIFY_API
--- Source/TestVTune/README.txt
+++
@@ -1,5 +0,0 @@
-﻿1. VTune的安裝目录下有include、lib32、lib64几个目录，将它们将到搜索路径中去；
-2. 链接库的输入里加上：libittnotify.lib；
-2. 包含头文件："ittnotify.h"；
-3. To be able to see user tasks in your results, enable the Analyze user tasks checkbox in analysis settings；
-4. API的用法可参考帮助文档：[User's Guide]、[API Support]；
