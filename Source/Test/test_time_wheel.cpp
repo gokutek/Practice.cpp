f6dd5461af8b2adcb68f1b6f477727b7421da394
shinra <vxer@qq.com>
2022-08-15 13:51:18 +0800

    时间轮

---
+++ Source/Test/test_time_wheel.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+class TimeWheel
+{
+public:
+	//注册定时器
+	int AddTimer();
+	//删除定时器
+	void RemoveTimer(int Handle);
+	//更新定时器
+	void UpdateTimer();
+
+private:
+};
+
+int TimeWheel::AddTimer()
+{
+	//TODO:
+	return -1;
+}
+
+void TimeWheel::RemoveTimer(int Handle)
+{
+	//TODO:
+}
+
+void TimeWheel::UpdateTimer()
+{
+	//TODO:
+}
+
+int main()
+{
+	return 0;
+}
