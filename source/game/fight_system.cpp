591c86c8fc7017734e4378926acdb08e9cf20c27
joy <vxer@qq.com>
2023-03-27 10:00:49 +0800

    test

--- source/game/fight_system.cpp
+++ source/game/fight_system.cpp
@@ -2,12 +2,17 @@
  * 2023/03/23: 战斗系统随想
  */
 
+struct attribute
+{
+	bool disable_input; //硬直
+};
+
 class player_object
 {
 public:
 };
 
-class skill_object
+class game_ability
 {
 public:
 	void on_end();
