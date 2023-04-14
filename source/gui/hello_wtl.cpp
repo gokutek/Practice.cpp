685c15d5a51af26bb475257cb279934ea8816c3c
joy <vxer@qq.com>
2023-04-14 18:07:57 +0800

    添加gui目录

---
+++ source/gui/hello_wtl.cpp
@@ -0,0 +1,12 @@
+#include <atlbase.h>
+#include <atlapp.h>
+#include <atlframe.h>
+#include <atlctrls.h>
+#include <atldlgs.h>
+#include <atlctrlw.h>
+#include <atltheme.h>
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR lpstrCmdLine, int nCmdShow)
+{
+    return 0;
+}
