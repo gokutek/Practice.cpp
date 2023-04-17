5cea208947d28b53fcf3cda773306ccc4e333db5
joy <vxer@qq.com>
2023-04-17 15:43:25 +0800

    wtl

--- source/gui/hello_wtl.cpp
+++ source/gui/hello_wtl.cpp
@@ -1,12 +1,62 @@
+/*
+ * 2023/04/17: 仅测试，后续UI还是考虑用Qt~
+ */
+
 #include <atlbase.h>
 #include <atlapp.h>
 #include <atlframe.h>
 #include <atlctrls.h>
-#include <atldlgs.h>
 #include <atlctrlw.h>
-#include <atltheme.h>
+#include <atldlgs.h>
+#include <atlscrl.h>
+#include <atlmisc.h>
+#include <atlprint.h>
+#include <atlcrack.h>
+
+CAppModule _Module;
+
+class CMainFrame : public CFrameWindowImpl<CMainFrame>
+{
+public:
+	DECLARE_FRAME_WND_CLASS(NULL, 0)
+};
+
+int Run(LPTSTR /*lpstrCmdLine*/ = NULL, int nCmdShow = SW_SHOWDEFAULT)
+{
+	CMessageLoop theLoop;
+	_Module.AddMessageLoop(&theLoop);
+
+	CMainFrame wndMain;
+
+	if (wndMain.CreateEx() == NULL)
+	{
+		ATLTRACE(_T("Main window creation failed!\n"));
+		return 0;
+	}
+
+	wndMain.ShowWindow(nCmdShow);
+
+	int nRet = theLoop.Run();
+
+	_Module.RemoveMessageLoop();
+	return nRet;
+}
 
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR lpstrCmdLine, int nCmdShow)
 {
-    return 0;
+	INITCOMMONCONTROLSEX iccx;
+	iccx.dwSize = sizeof(iccx);
+	iccx.dwICC = ICC_COOL_CLASSES | ICC_BAR_CLASSES;
+	BOOL bRet = ::InitCommonControlsEx(&iccx);
+	bRet;
+	ATLASSERT(bRet);
+
+	HRESULT hRes = _Module.Init(NULL, hInstance);
+	hRes;
+	ATLASSERT(SUCCEEDED(hRes));
+
+	int nRet = Run(lpstrCmdLine, nCmdShow);
+
+	_Module.Term();
+	return nRet;
 }
