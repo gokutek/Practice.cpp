2bacc7b07de651a5ace0ac11806aeb4caee69f5e
joy <vxer@qq.com>
2023-11-21 17:22:50 +0800

    udp测试占坑

---
+++ source/test/test_udp_socket.cpp
@@ -0,0 +1,24 @@
+﻿/*
+ * 创建时间: 2023/11/15
+ */
+
+#include <WinSock2.h>
+#include <Windows.h>
+
+static void udp_server_main()
+{
+}
+
+static void udp_client_main()
+{
+}
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		return 0;
+	}
+
+	return 0;
+}
