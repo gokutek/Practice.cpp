3de43f1ce296b9faf69ff1a0dcf4a77a21254828
joy <vxer@qq.com>
2023-12-07 11:33:05 +0800

    printf

---
+++ source/test/test_printf_format.cpp
@@ -0,0 +1,25 @@
+﻿#include <string.h>
+#include <stdio.h>
+#include <iostream>
+#include <assert.h>
+
+int main()
+{
+	char buffer[256];
+
+	sprintf_s(buffer, "%d", 100);
+	assert(!strcmp(buffer, "100"));
+
+#pragma warning(push)
+#pragma warning(disable: 4477)
+	//类型不匹配，并不会先将float转成int再输出
+	sprintf_s(buffer, "%d", 100.0f);
+	assert(!strcmp(buffer, "0"));
+
+	//同理，这里也不会将int转成float再输出
+	sprintf_s(buffer, "%f", 100);
+	assert(strcmp(buffer, "100"));
+#pragma warning(pop)
+	
+	return 0;
+}
