2ee9c64d49ad8eb18eb954f8910a54a1e7325535
joy <vxer@qq.com>
2023-12-22 10:58:55 +0800

    test_tagged_pointer

---
+++ source/test/test_tagged_pointer.cpp
@@ -0,0 +1,46 @@
+﻿/*
+===============================================================================
+创建时间：2023/12/22
+说明：在看pbrt的代码时发现，它里面用TaggedPointer实现了类似继承的“效果”。
+===============================================================================
+*/
+
+#include <iostream>
+
+class TaggedPointer
+{
+public:
+	template<typename T>
+	TaggedPointer(T* ptr)
+	{
+	}
+
+	//TODO: dispatch
+};
+
+class Shape : public TaggedPointer
+{
+public:
+	using TaggedPointer::TaggedPointer;
+
+	void Render()
+	{
+		//Distach
+	}
+};
+
+class Sphere
+{
+public:
+	void Render()
+	{
+		std::cout << "Sphere::Render" << std::endl;
+	}
+};
+
+int main()
+{
+	Shape sphere = new Sphere();
+
+	return 0;
+}
